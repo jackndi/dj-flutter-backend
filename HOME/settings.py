9a46dcd559ab2125dec7716158456779b0fccb9c
Jackson Kabuku <ndirangu240@gmail.com>
2020-06-29 00:27:50 +0200

    added cors to the project

--- HOME/settings.py
+++ HOME/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     # Third party packages
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,7 @@ REST_FRAMEWORK = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 django_heroku.settings(locals())
+
+
+
+CORS_ORIGIN_ALLOW_ALL = True
