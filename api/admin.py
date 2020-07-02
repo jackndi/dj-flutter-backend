9519823bcb25ce2136f3d77c6b2a5702541bfa97
Jackson Kabuku <ndirangu240@gmail.com>
2020-07-03 01:09:41 +0200

    registered models in the admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import  models
+
+
+admin.site.register(models.Course)
+admin.site.register(models.TrackableItem)
+admin.site.register(models.QRCode)
+admin.site.register(models.TimeRecord)
+admin.site.register(models.CourseUser)
